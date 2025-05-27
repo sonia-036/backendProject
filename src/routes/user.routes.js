826dd2be7888a1d22d1f0feda4715535ccf367ef
sonia-036 <sonia362003@gmail.com>
2025-05-27 15:39:13 +0530

    router and controller and refreshToken

---
+++ src/routes/user.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { loginUser, logoutUser, registerUser, refreshAcessToken } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([{
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
+router.routr("/refresh-token").post(refreshAcessToken)
+
+export default router
